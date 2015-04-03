5cd14101ef84bb1f5d21c8e27e439ad31ffd95c7
WaterPistols <adam@civitronic.ro>
2015-04-03 19:54:30 +0300

    link

---
+++ com.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<?
+$inputuser = $_POST
+</head>
+<body>
+
+<form>
+First name:<br>
+<input type="text" name="firstname">
+<br>
+Last name:<br>
+<input type="text" name="lastname">
+</form>
+
+</body>
+</html>
--- model/administrare/admin_barajos.php
+++
@@ -1,8 +0,0 @@
-
-</div>
-<div class="barajos"><p>Copyright Grelus Andrei</p></div>
-</div>
-
-</body>
-</html>
-
